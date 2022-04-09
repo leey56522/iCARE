e415598000304f25d2e691dfe9df2b3c83d71d20
Lucia <pongu80@gmail.com>
2022-04-09 15:32:45 -0500

    the popup window now appears in the center of the screen

--- popup.js
+++ popup.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('button').addEventListener('click', alertHello)
+    document.querySelector('button').addEventListener('click', popWindow)
 })
 
-function alertHello() {
+function popWindow() {
+    // Sets popup window height and width
+    const window_height = 800;
+    const window_width = 1000;
+
+    // Set the window to pop up in the center of the screen
+    const x = screen.width/2 - window_width/2;
+    const y = screen.height/2 - window_width/2;
+
     chrome.windows.create({
         url:'/popup.html',
         type: "popup",
-        height: 800,
-        width: 1000,
+        width: window_width,
+        height: window_height,
+        left: x,
+        top: -y
     });
-
 }
