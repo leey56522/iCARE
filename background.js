30f9cc7327e812d82b795a5a43dd81b687678a5f
Lucia <pongu80@gmail.com>
2022-04-09 19:46:01 -0500

    Updated repeating interval function

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
-console.log('background is running')
-
-// The default time will be 30 minutes
-let defaultDuration = 1.0;
+// The default time interval will be 30 minutes
+let defaultDuration = 20.0;
 
 // Creates an alarm after above the above duration is past
-chrome.alarms.create("exercise time", {delayInMinutes: defaultDuration});
+function createAlarm() {
+    chrome.alarms.create("exercise time", {delayInMinutes: defaultDuration, periodInMinutes: defaultDuration});
+}
 
-// Makes the alarm call on a function after the duration passes
+// Makes the alarm call on a window creating function after the duration passes
 chrome.alarms.onAlarm.addListener(function() {
-    console.log('this is an alarm')
     popWindow()
 })
 
@@ -19,7 +18,7 @@ function popWindow() {
 
     // x and y coordinates are calculated based on the screen size and window size
     const x = screen.width/2 - window_width/2;
-    const y = screen.height/2 - window_width/2;
+    const y = screen.height/2 - window_height/2;
 
     chrome.windows.create({
         url:'/window.html',
@@ -30,3 +29,15 @@ function popWindow() {
         top: -y
     });
 }
+
+createAlarm()
+
+// Updates the interval based on set time input
+chrome.runtime.onMessage.addListener(
+    function(request, sendResponse) {
+        console.log('Event received in background page')
+        defaultDuration = request.newTime * 1.0;
+        createAlarm();
+        sendResponse({success: true});
+    }
+  );
--- popup.js
+++ popup.js
@@ -4,9 +4,15 @@ document.querySelector('#change-interval').addEventListener('click', changeInter
 // Saves the input value to local storage
 function changeInterval() {
     const newTime = parseInt(document.querySelector('input').value);
+    // Checks if the input is a number. If yes, a part of the innerHTML of interface changes
     if(isNaN(newTime)) {
         alert('Make sure your input is a whole number!')
     } else {
         document.querySelector('.current-interval').innerHTML = `<p>The current interval is every ${newTime} minutes</p>`
+        // testing message passing function 
+        chrome.runtime.sendMessage({newTime}, function(response) {
+            console.log(response);
+            console.log(newTime)
+        })
     }
 }
