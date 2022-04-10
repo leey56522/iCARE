ff410197d7deda9634ba61fb406dd2b0fc0d5842
Lucia <pongu80@gmail.com>
2022-04-10 02:25:13 -0500

    added random gif/image selector

--- option.js
+++ option.js
@@ -7,7 +7,7 @@ function changeInterval() {
     // Checks if the input is a number and above 0. If yes, a part of the innerHTML of interface changes
     if(isNaN(newTime)) {
         alert('Make sure your input is a whole number!')
-    } else if(newTime == 0) {
+    } else if(newTime <= 0) {
         alert('Please enter a number greater than 0!')
     } else {
         // pass the input value to the background js
