ef1d41ab3ff7d51b0df6fb9aae5b622cf252e8df
Lucia <pongu80@gmail.com>
2022-04-11 09:46:20 -0500

    fixed getRandomNum function

--- window.js
+++ window.js
@@ -1,9 +1,9 @@
 // Returns a random number that corresponds to the gif name
 function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max-min) + min);
+    return Math.floor(Math.random() * max) + min;
 }
 
-let imageNum = getRandomNum(1, 12);
+let imageNum = getRandomNum(1, 11);
 
 // Random gif is selected to pop up every time interval
 document.querySelector('#img-box').innerHTML = `<img src="./img/${imageNum}.gif" alt="eye or neck exercise">`
