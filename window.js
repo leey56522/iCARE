cee85f2ef730d8199c33267e9253dfc29e77ad6b
Lucia <pongu80@gmail.com>
2022-04-18 19:26:55 -0500

    bug fix for out-of-range images

--- window.js
+++ window.js
@@ -4,9 +4,17 @@ function getRandomNum(min, max) {
 }
 
 let imageNum = getRandomNum(1, 11);
+let imagePath;
+
+if (imageNum < 6) {
+    imagePath = `<img src="./img/${imageNum}.gif" alt="eye or neck exercise">`
+} else {
+    imagePath = `<img src="./img/${imageNum}.png" alt="eye or neck exercise">`
+}
+
 
 // Random gif is selected to pop up every time interval
-document.querySelector('#img-box').innerHTML = `<img src="./img/${imageNum}.gif" alt="eye or neck exercise">`
+document.querySelector('#img-box').innerHTML = imagePath;
 
 // Automatically closes the popup window after 45 seconds
 setTimeout(function() {
